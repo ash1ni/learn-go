831b708011ad6d2c6a7249a15d35db108dcdeea3
Ashwini Kumar Pandey <itsashwani001@gmail.com>
2023-04-15 22:45:11 +0530

    first commit

---
+++ values.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+func main(){
+	fmt.Println("hello" + "friend")
+	fmt.Println("1+1 = ", 1+1)
+	fmt.Println(true&&false)
+	fmt.Println(true||false)
+	fmt.Println(!true)
+	
+
+}
