1ec2d08dd82a6d81b27041975031777e79982889
Ashwini Kumar Pandey <itsashwani001@gmail.com>
2023-04-15 22:57:05 +0530

    variables in go

---
+++ variable.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+func main(){
+	var a = "initial"
+	fmt.Println(a)
+
+	b:="variable b"
+	fmt.Println(b)
+
+	var c, d int = 1,2
+	fmt.Println(c,d)
+
+	var e int
+	fmt.Println(e)
+}
